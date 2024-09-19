4ac617120ed7bae9f710ee0ebb6ecef28c8838ae
Damián Roháček <rohacekdamian@seznam.cz>
2024-09-19 10:40:59 +0200

    dnešní dvouhodinovka
    jel jsem napřed

---
+++ NahodneCislo/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NahodneCislo.Models;
+
+namespace NahodneCislo.Controllers
+{
+    public class HomeController : Controller //třída dědí vlastnosti od třídy controller
+    {
+        public IActionResult Index() {
+            Generator generator = new Generator();
+            /*způsoby, jak přenést data
+            ViewData - na způsob dictionary
+            ViewBag - kolekce využívající dynamičnosti .netu, ukládáme do vlastností
+            TempData - předání dat u přesměrování, po konci requestu se mažou
+            */
+            ViewBag.Cislo = generator.VratCislo();
+            return View();
+        }
+    }
+}
